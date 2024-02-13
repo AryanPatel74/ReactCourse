1a55a5fc7f75e41fb26d4b1a390db9bdc5521cf8
unknown <amanbbkup74@gmail.com>
2024-02-14 02:53:57 +0530

    Section 1 Code

---
+++ app.js
@@ -0,0 +1,13 @@
+const header = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement(
+      "h1",
+      { id: "heading" },
+      "Hello World! Welcome to React course 🚀"
+    ),
+    React.createElement("h2", {}, "this is h2 tag"),
+  ])
+);
+ReactDOM.render(header, document.getElementById("root"));
